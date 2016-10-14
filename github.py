1c06a58e616cfaa84203150b8a807ec165a42d9c
jawadfaisal <jfaisal.knysys@gmail.com>
2016-10-14 18:05:57 +0500

    1
    resolved

--- github.py
+++
@@ -1,9 +0,0 @@
-from github import Github
-
-# First create a Github instance:
-g = Github("jfaisal.knysys@gmail.com", "knysys@123")
-
-# Then play with your Github objects:
-for repo in g.get_user().get_repos():
-    print repo.name
-    repo.edit(has_wiki=False)
